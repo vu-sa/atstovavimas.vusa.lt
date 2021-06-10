42d18b86a793b8627fa4320078aa0062769c727d
Justinas Kavoliūnas <justinaskav@gmail.com>
2021-06-11 01:51:37 +0300

    Fixed nav

--- resources/views/elements/nav.blade.php
+++ resources/views/elements/nav.blade.php
@@ -7,26 +7,43 @@
         <div class="collapse navbar-collapse vertical-nav px-3 py-3" id="navbarToggler">
             <ul class="navbar-nav navbar-nav-scroll me-auto mb-2 mb-lg-0 flex-column">
                 <li class="navbar-text">
-                    <a class="nav-link" style="font-size: 1.2em" aria-current="page"><img src="img/vusa.png" width="50" style="padding-right:15px;margin:0"><strong>Atstovavimo gidas</strong></a>
-                </li>
-                <li class="navbar-text">
-                    <a class="nav-link" style="font-size: 1em" aria-current="page" href="#ivadas"><strong>Įvadas</strong></a>
+                    <a class="nav-link" style="font-size: 1.2em" aria-current="page" href="#ivadas"><img src="img/vusa.png" width="50" style="padding-right:15px;margin:0"><strong>Atstovavimo gidas</strong></a>
                 </li>
                 <div class="dropdown-divider"></div>
                 <li class="navbar-text">
                     <a style="font-size:1em" class="nav-link"><strong>I dalis</strong></a>
                   </li>
-                @include('elements.nav.AM')
+                @include('elements.nav.schemos')
+                <li class="navbar-link">
+                    <a class="nav-link" aria-current="page" href="#KAP">Kamieninis akademinis padalinys</a>
+                </li>
+                <li class="navbar-link">
+                    <a class="nav-link" aria-current="page" href="#VU">Vilniaus universitetas</a>
+                </li>
+                <li class="navbar-link">
+                    <a class="nav-link" aria-current="page" href="#aukstasis-mokslas">Aukštasis mokslas</a>
+                </li>
+                <li class="navbar-link">
+                    <a class="nav-link" aria-current="page" href="#vyriausybe">Vyriausybė</a>
+                </li>
+                <li class="navbar-link">
+                    <a class="nav-link" aria-current="page" href="#smsm">ŠMSM</a>
+                </li>
+                <li class="navbar-link">
+                    <a class="nav-link" aria-current="page" href="#procesai">Įstatymų leidyba</a>
+                </li>
+                {{-- @include('elements.nav.AM')
                 @include('elements.nav.Seimas')
                 @include('elements.nav.LRV')
                 @include('elements.nav.SMSM')
-                @include('elements.nav.procesai')
+                @include('elements.nav.procesai') --}}
                 <div class="dropdown-divider"></div>
+
                 <li class="navbar-text">
                     <a style="font-size:1em" class="nav-link"><strong>II dalis</strong></a>
                   </li>
-                  @include('elements.nav.SAO')
-                  @include('elements.nav.atstovuKompetencijos')
+                @include('elements.nav.SAO')
+                @include('elements.nav.atstovuKompetencijos')
                   
                 <div class="dropdown-divider"></div>
                 <li class="nav-item">
---
+++ resources/views/elements/nav/schemos.blade.php
@@ -0,0 +1,14 @@
+<li class="nav-item dropdown dropend">
+    <a class="nav-link dropdown-toggle" href="#vusa" id="navbarDropdownMenuLink" role="button"
+        data-bs-toggle="dropdown" aria-expanded="false">
+        <strong>Interaktyvios schemos</strong>
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <li><a class="dropdown-item" href="#kap-schema">VU KAP</a></li>
+        <li><a class="dropdown-item" href="#VU-schema">Vilniaus universitetas</a></li>
+        <li><a class="dropdown-item" href="#aukstojo-mokslo-schema">Aukštasis mokslas</a></li>
+        <li><a class="dropdown-item" href="#vyriausybe">Vyriausybė</a></li>
+        <li><a class="dropdown-item" href="#smsm">ŠMSM</a></li>
+        <li><a class="dropdown-item" href="#seimo-nutarimu-trumpoji-schema">Įstatymų leidyba</a></li>
+    </ul>
+</li>
