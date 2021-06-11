08a2466c4cce719c9fb19b529d9cee099ed2a408
Justinas Kavoliūnas <justinaskav@gmail.com>
2021-06-11 17:07:09 +0300

    Updated quiz1

--- resources/views/quizzes/quiz1.blade.php
+++ resources/views/quizzes/quiz1.blade.php
@@ -1,35 +1,35 @@
 @extends('quizzes.template')
 
 @section("html-title")
-<title>Klausimai apie AM</title>
+<title>Klausimai apie VU KAP</title>
 @endsection
 
 @section("quiz-title")
-    <h2>Pasitikrink žinias apie aukštojo mokslo sistemą!</h2>
+    <h2>Pasitikrink žinias apie VU kamieninius akademinius padalinius!</h2>
 @endsection
 
 @section("quiz-questions")
 <script>
 var questions = [{
-    question: "What is 2*5?",
-    choices: [2, 5, 10, 15, 20],
-    correctAnswer: 2
-}, {
-    question: "What is 3*6?",
-    choices: [3, 6, 9, 12, 18],
-    correctAnswer: 4
-}, {
-    question: "What is 8*9?",
-    choices: [72, 99, 108, 134, 156],
-    correctAnswer: 0
-}, {
-    question: "What is 1*7?",
-    choices: [4, 5, 6, 7, 8],
+    question: "Kurį iš šių vienasmenių VU KAP sprendimų priėmėjų renka KAP darbuotojai?",
+    choices: ['KAP vadovą', 'ŠAP vadovą', 'KAP Tarybos pirmininką', 'Nei vieno iš jų'],
     correctAnswer: 3
 }, {
-    question: "What is 8*8?",
-    choices: [20, 30, 40, 50, 64],
-    correctAnswer: 4
+    question: "Kuris KAP organas padeda suformuoti su studijomis susijusius sprendimus Pavaduotojui studijoms?",
+    choices: ['SPK', 'Studijų kolegija', 'Doktorantūros mokykla'],
+    correctAnswer: 1
+}, {
+    question: "Kuriame iš šių KAP darinių nėra studentų atstovo?",
+    choices: ['Rinkimų komisijoje', 'Dekanate', 'ŠAP'],
+    correctAnswer: 2
+}, {
+    question: "Kas gali nuspręsti sudaryti laikinąją darbo grupę KAP studijų programų strategijai parengti?",
+    choices: ['KAP Tarybos pirmininkas', 'KAP vadovo pavaduotojas studijų organizavimui', 'KAP vadovas'],
+    correctAnswer: 2
+}, {
+    question: "Kas sprendžia Šakiniame akademiniame padalinyje su moksliniais tyrimais dirbančio mokslininko sutarties su Universitetu pratęsimo klausimą?",
+    choices: ['KAP Priėmimo-atestacinė komisija', 'ŠAP vadovas', 'KAP vadovas'],
+    correctAnswer: 0
 }];
 
 </script>
